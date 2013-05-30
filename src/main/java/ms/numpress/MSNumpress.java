1955e0bec04225d3ea8d5281a1c7a9ef0b3cec5f
Johan Teleman <johan.teleman@immun.lth.se>
2013-05-30 14:32:07 +0200

    updated java library with intensity algorithms to reflect cpp impl.

---
+++ src/main/java/ms/numpress/MSNumpress.java
@@ -0,0 +1,290 @@
+package ms.numpress;
+
+class MSNumpress {
+
+
+	protected static double ENC_LINEAR_FIXED_POINT = 100000.0;
+	protected static double ENC_TWO_BYTE_FIXED_POINT = 3000.0;
+	/**
+	 * Encodes the int x as a number of halfbytes in res. 
+	 * res_length is incremented by the number of halfbytes, 
+	 * which will be 1 <= n <= 9
+	 */
+	protected static int encodeInt(
+			long x,
+			byte[] res,
+			int resOffset
+	) {
+		byte i, l;
+		long m;
+		long mask = 0xf0000000;
+		long init = x & mask;
+
+		if (init == 0) {
+			l = 8;
+			for (i=0; i<8; i++) {
+				m = mask >> (4*i);
+				if ((x & m) != 0) {
+					l = i;
+					break;
+				}
+			}
+			res[resOffset] = l;
+			for (i=l; i<8; i++) 
+				res[resOffset+1+i-l] = (byte)(0xf & (x >> (4*(i-l))));
+			
+			return 1+8-l;
+
+		} else if (init == mask) {
+			l = 7;
+			for (i=0; i<8; i++) {
+				m = mask >> (4*i);
+				if ((x & m) != m) {
+					l = i;
+					break;
+				}
+			}
+			res[resOffset] = (byte)(l | 8);
+			for (i=l; i<8; i++) 
+				res[resOffset+1+i-l] = (byte)(0xf & (x >> (4*(i-l))));
+			
+			return 1+8-l;
+
+		} else {
+			res[resOffset] = 0;
+			for (i=0; i<8; i++)
+				res[resOffset+1+i] = (byte)(0xf & (x >> (4*i)));
+			
+			return 9;
+
+		}
+	}
+	
+	
+	/**
+	 * Encodes the doubles in data by first using a 
+	 *   - lossy conversion to a 4 byte 5 decimal fixed point repressentation
+	 *   - storing the residuals from a linear prediction after first to values
+	 *   - encoding by encodeInt (see above) 
+	 * 
+	 * The resulting binary is maximally dataSize * 5 bytes, but much less if the 
+	 * data is reasonably smooth on the first order.
+	 *
+	 * This encoding is suitable for typical m/z or retention time binary arrays. 
+	 * For masses above 100 m/z the encoding is accurate to at least 0.1 ppm.
+	 */
+	public static int encodeLinear(
+			double[] data, 
+			int dataSize, 
+			byte[] result
+	) {
+		long[] ints = new long[3];
+		int i;
+		int ri = 8;
+		byte halfBytes[] = new byte[10];
+		int halfByteCount = 0;
+		int hbi;
+		long extrapol;
+		long diff;
+
+		ints[1] = (long)(data[0] * ENC_LINEAR_FIXED_POINT + 0.5);
+		ints[2] = (long)(data[1] * ENC_LINEAR_FIXED_POINT + 0.5);
+
+		for (i=0; i<4; i++) {
+			result[i] 	= (byte)((ints[1] >> (i*8)) & 0xff);
+			result[4+i] = (byte)((ints[2] >> (i*8)) & 0xff);
+		}
+
+		for (i=2; i<dataSize; i++) {
+			ints[0] = ints[1];
+			ints[1] = ints[2];
+			ints[2] = (long)(data[i] * ENC_LINEAR_FIXED_POINT + 0.5);
+			extrapol = ints[1] + (ints[1] - ints[0]);
+			diff = ints[2] - extrapol;
+			halfByteCount += encodeInt(diff, halfBytes, halfByteCount);
+					
+			for (hbi=1; hbi < halfByteCount; hbi+=2)
+				result[ri++] = (byte)((halfBytes[hbi-1] << 4) | (halfBytes[hbi] & 0xf));
+			
+			if (halfByteCount % 2 != 0) {
+				halfBytes[0] = halfBytes[halfByteCount-1];
+				halfByteCount = 1;
+			} else 
+				halfByteCount = 0;
+
+		}
+		if (halfByteCount == 1)
+			result[ri++] = (byte)(halfBytes[0] << 4);
+
+		return ri;
+	}
+	
+	
+	
+	/**
+	 * Decodes data encoded by encodeLinear. Note that the compression 
+	 * discard any information < 1e-5, so data is only guaranteed 
+	 * to be within +- 5e-6 of the original value.
+	 *
+	 * Further, values > ~42000 will also be truncated because of the
+	 * fixed point representation, so this scheme is stronly discouraged 
+	 * if values above might be above this size.
+	 *
+	 * result vector guaranteedly shorter than twice the data length (in nbr of values)
+	 * returns the number of doubles read
+	 */
+	public static int decodeLinear(
+			byte[] data, 
+			int dataSize, 
+			double[] result
+	) {
+		int ri = 2;
+		long[] ints = new long[3];
+		long extrapol;
+		long y;
+		IntDecoder dec = new IntDecoder(data, 8);
+
+		ints[1] = 0;
+		ints[2] = 0;
+
+		for (int i=0; i<4; i++) {
+			ints[1] = ints[1] | ( (0xFF & data[i]) << (i*8));
+			ints[2] = ints[2] | ( (0xFF & data[4+i]) << (i*8));
+		}
+		
+		result[0] = ints[1] / ENC_LINEAR_FIXED_POINT;
+		result[1] = ints[2] / ENC_LINEAR_FIXED_POINT;
+	
+		while (dec.pos < dataSize) {
+			if (dec.pos == (dataSize - 1) && dec.half)
+				if ((data[dec.pos] & 0xf) != 0x8)
+					break;
+			
+			ints[0] = ints[1];
+			ints[1] = ints[2];
+			ints[2] = dec.next();
+			
+			extrapol = ints[1] + (ints[1] - ints[0]);
+			y = extrapol + ints[2];
+			result[ri++] = y / ENC_LINEAR_FIXED_POINT;
+			ints[2] = y;
+		}
+		
+		return ri;
+	}
+	
+	
+	/**
+	 * Encodes ion counts by simply rounding to the nearest 4 byte integer, 
+	 * and compressing each integer with encodeInt. 
+	 *
+	 * The handleable range is therefore 0 -> 4294967294.
+	 * The resulting binary is maximally dataSize * 5 bytes, but much less if the 
+	 * data is close to 0 on average.
+	 */
+	public static int encodeCount(
+			double[] data, 
+			int dataSize, 
+			byte[] result
+	) {
+		long count;
+		int ri = 0;
+		int hbi = 0;
+		byte halfBytes[] = new byte[10];
+		int halfByteCount = 0;
+
+		//printf("Encoding %d doubles\n", (int)dataSize);
+
+		for (int i=0; i<dataSize; i++) {
+			count = (long)(data[i] + 0.5);
+			halfByteCount += encodeInt(count, halfBytes, halfByteCount);
+					
+			for (hbi=1; hbi < halfByteCount; hbi+=2)
+				result[ri++] = (byte)((halfBytes[hbi-1] << 4) | (halfBytes[hbi] & 0xf));
+			
+			if (halfByteCount % 2 != 0) {
+				halfBytes[0] = halfBytes[halfByteCount-1];
+				halfByteCount = 1;
+			} else
+				halfByteCount = 0;
+
+		}
+		if (halfByteCount == 1)
+			result[ri++] = (byte)(halfBytes[0] << 4);
+		
+		return ri;
+	}
+	
+	
+	/**
+	 * Decodes data encoded by encodeCount
+	 *
+	 * result vector guaranteedly shorter than twice the data length (in nbr of values)
+	 * returns the number of doubles read
+	 */
+	public static int decodeCount(
+			byte[] data, 
+			int dataSize, 
+			double[] result
+	) {
+		int ri = 0;
+		long count;
+		IntDecoder dec = new IntDecoder(data, 0);
+
+		while (dec.pos < dataSize) {
+			if (dec.pos == (dataSize - 1) && dec.half)
+				if ((data[dec.pos] & 0xf) != 0x8)
+					break;
+				
+			count = dec.next();
+			result[ri++] = count;
+		}
+		return ri;
+	}
+	
+	
+	/**
+	 * Encodes ion counts by taking the natural logarithm, and storing a
+	 * fixed point representation of this. This is calculated as
+	 * 
+	 * unsigned short fp = log(d) * 3000.0 + 0.5
+	 */
+	public static int encode2ByteFloat(
+			double[] data, 
+			int dataSize, 
+			byte[] result
+	) {
+		int fp;
+		int ri = 0;
+		
+		for (int i=0; i<dataSize; i++) {
+			fp = (int)(Math.log(data[i]) * ENC_TWO_BYTE_FIXED_POINT + 0.5);
+		
+			result[ri++] = (byte)(0xff & fp);
+			result[ri++] = (byte)(fp >> 8);
+		}
+		return ri;
+	}
+	
+	
+	/**
+	 * Decodes data encoded by encode2ByteFloat
+	 *
+	 * result vector length is twice the data length
+	 * returns the number of doubles read
+	 */
+	public static int decode2ByteFloat(
+			byte[] data, 
+			int dataSize, 
+			double[] result
+	) {
+		int fp;
+		int ri = 0;
+	
+		for (int i=0; i<dataSize; i+=2) {
+			fp = (0xff & data[i]) | ((0xff & data[i+1]) << 8);
+			result[ri++] = Math.exp(((double)(0xffff & fp)) / ENC_TWO_BYTE_FIXED_POINT);
+		}
+		return ri;
+	}
+}
---
+++ src/test/java/ms/numpress/MSNumpressTest.java
@@ -0,0 +1,172 @@
+package ms.numpress;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MSNumpressTest {
+
+	@Test
+	public void encodeInt() {
+		byte[] res = new byte[10];
+		int l;
+		l = MSNumpress.encodeInt(0l, res, 0);
+		assertEquals(1, l);
+		assertEquals(8, res[0] & 0xf);
+		
+		l = MSNumpress.encodeInt(-1l, res, 0);
+		assertEquals(2, l);
+		assertEquals(0xf, res[0] & 0xf);
+		assertEquals(0xf, res[1] & 0xf);
+		
+		l = MSNumpress.encodeInt(35l, res, 0);
+		assertEquals(3, l);
+		assertEquals(6, res[0] & 0xf);
+		assertEquals(0x3, res[1] & 0xf);
+		assertEquals(0x2, res[2] & 0xf);
+		
+		l = MSNumpress.encodeInt(370000000l, res, 0);
+		assertEquals(9, l);
+		assertEquals(0, res[0] & 0xf);
+		assertEquals(0x0, res[1] & 0xf);
+		assertEquals(0x8, res[2] & 0xf);
+		assertEquals(0x0, res[3] & 0xf);
+		assertEquals(0xc, res[4] & 0xf);
+		assertEquals(0xd, res[5] & 0xf);
+		assertEquals(0x0, res[6] & 0xf);
+		assertEquals(0x6, res[7] & 0xf);
+		assertEquals(0x1, res[8] & 0xf);
+	}
+	
+	
+	@Test
+	public void decodeInt() {
+		byte[] res = new byte[10];
+		res[0] = (byte)0x75;
+		res[1] = (byte)0x87;
+		res[2] = (byte)0x10;
+		res[3] = (byte)0x08;
+		res[4] = (byte)0x0c;
+		res[5] = (byte)0xd0;
+		res[6] = (byte)0x61;
+		IntDecoder dec = new IntDecoder(res, 0);
+		
+		long l;
+		l = dec.next();
+		assertEquals(5, l);
+		
+		l = dec.next();
+		assertEquals(0, l);
+		
+		l = dec.next();
+		assertEquals(1, l);
+		
+		l = dec.next();
+		assertEquals(370000000L, l);
+	}
+
+	@Test
+	public void encodeLinear() {
+		double[] mzs = {100.0, 200.0, 300.00005, 400.00010};
+		byte[] encoded 		= new byte[16];
+		int encodedBytes 	= MSNumpress.encodeLinear(mzs, 4, encoded);
+		assertEquals(10, encodedBytes);
+		assertEquals(0x80, 0xff & encoded[0]);
+		assertEquals(0x96, 0xff & encoded[1]);
+		assertEquals(0x98, 0xff & encoded[2]);
+		assertEquals(0x00, 0xff & encoded[3]);
+		assertEquals(0x75, 0xff & encoded[8]);
+		assertEquals(0x80, 0xf0 & encoded[9]);
+	}
+	
+	
+	@Test
+	public void decodeLinearNice() {
+		double[] mzs = {100.0, 200.0, 300.00005, 400.00010};
+		byte[] encoded 		= new byte[20];
+		int encodedBytes 	= MSNumpress.encodeLinear(mzs, 4, encoded);
+		double[] decoded 	= new double[4];
+		int numDecoded 		= MSNumpress.decodeLinear(encoded, encodedBytes, decoded);
+		assertEquals(4, numDecoded);
+		assertEquals(100.0, decoded[0], 0.000005);
+		assertEquals(200.0, decoded[1], 0.000005);
+		assertEquals(300.00005, decoded[2], 0.000005);
+		assertEquals(400.00010, decoded[3], 0.000005);
+	}
+	
+	
+	@Test
+	public void decodeLinearWierd() {
+		double[] mzs = {100.0, 200.0, 4000.00005, 0.00010};
+		byte[] encoded 		= new byte[20];
+		int encodedBytes 	= MSNumpress.encodeLinear(mzs, 4, encoded);
+		double[] decoded 	= new double[4];
+		int numDecoded 		= MSNumpress.decodeLinear(encoded, encodedBytes, decoded);
+		assertEquals(4, numDecoded);
+		assertEquals(100.0, decoded[0], 0.000005);
+		assertEquals(200.0, decoded[1], 0.000005);
+		assertEquals(4000.00005, decoded[2], 0.000005);
+		assertEquals(0.00010, decoded[3], 0.000005);
+	}
+
+
+	@Test
+	public void encodeDecodeLinear() {
+		
+		int n = 1000;
+		double[] mzs = new double[n];
+		mzs[0] = 300 + Math.random();
+		for (int i=1; i<n; i++) 
+			mzs[i] = mzs[i-1] + Math.random();
+		
+		byte[] encoded 		= new byte[n * 5];
+		int encodedBytes 	= MSNumpress.encodeLinear(mzs, n, encoded);
+		double[] decoded 	= new double[n];
+		int decodedDoubles 	= MSNumpress.decodeLinear(encoded, encodedBytes, decoded);
+		
+		assertEquals(n, decodedDoubles);
+		
+		for (int i=0; i<n; i++) 
+			assertEquals(mzs[i], decoded[i], 0.000005);
+	}
+
+
+	@Test
+	public void encodeDecodeCount() {
+		
+		int n = 1000;
+		double[] ics = new double[n];
+		for (int i=0; i<n; i++) 
+			ics[i] = Math.pow(10, 6*Math.random());
+		
+		byte[] encoded 		= new byte[n * 5];
+		int encodedBytes 	= MSNumpress.encodeCount(ics, n, encoded);
+		double[] decoded 	= new double[n];
+		int decodedDoubles 	= MSNumpress.decodeCount(encoded, encodedBytes, decoded);
+		
+		assertEquals(n, decodedDoubles);
+		
+		for (int i=0; i<n; i++) 
+			assertEquals(ics[i], decoded[i], 0.5);
+	}
+
+
+	@Test
+	public void encodeDecode2ByteFloat() {
+		
+		int n = 1000;
+		double[] ics = new double[n];
+		for (int i=0; i<n; i++)
+			ics[i] = Math.pow(10, 6*Math.random());
+		
+		byte[] encoded 		= new byte[n * 5];
+		int encodedBytes 	= MSNumpress.encode2ByteFloat(ics, n, encoded);
+		double[] decoded 	= new double[n];
+		int decodedDoubles 	= MSNumpress.decode2ByteFloat(encoded, encodedBytes, decoded);
+		
+		assertEquals(n, decodedDoubles);
+		
+		for (int i=0; i<n; i++)
+			assertEquals(0.0, (ics[i] - decoded[i]) / ((ics[i] + decoded[i])/2), 0.0005);
+	}
+}
