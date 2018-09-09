b3f868ad830ed2a3af630fb7b12760f4ea611e71
Jan <rudolph@biochem.mpg.de>
2018-09-09 17:08:29 +0200

    nuget pack specify msbuild path

--- src/main/csharp/deploy.sh
+++ src/main/csharp/deploy.sh
@@ -4,5 +4,5 @@ CURRENT_TAG=$(git describe --abbrev=0 --tags)
 COMMITS_SINCE_TAG=$(git rev-list $CURRENT_TAG.. --count)
 VERSION_NUMBER=$(echo $CURRENT_TAG.$COMMITS_SINCE_TAG | sed 's/MsNumpress-//')
 
-nuget pack MSNumpress.csproj -Verbosity detailed -Prop Configuration=Release -Version $VERSION_NUMBER
+nuget pack MSNumpress.csproj -Verbosity detailed -Prop Configuration=Release -Version $VERSION_NUMBER -MSBuildPath /usr/lib/mono/msbuild/15.0/bin/
 nuget push *.nupkg -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json -Verbosity detailed
